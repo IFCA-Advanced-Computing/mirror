90f569df9593e081423a0ad8d5aa34f6ebf763e2
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2020-11-03 09:15:28 +0100

    Handle mirror data nicely

--- mirror/__init__.py
+++ mirror/__init__.py
@@ -1,3 +1,7 @@
 import pathlib
 
-MIRROR_DATA = pathlib.Path(".") / "mirror-data.yaml"
+MIRROR_DATA = pathlib.Path(__file__).parent
+MIRROR_DATA = MIRROR_DATA / ".." / "data" / "mirror-data.yaml"
+
+if not MIRROR_DATA.parent.exists():
+    MIRROR_DATA.parent.mkdir(parents=True)
